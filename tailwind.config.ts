540d367b5724fec2553550eff572f816b2a21b34
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-12-12 13:15:13 +0700

    feat: :bento: update shadcn/ui to nect (svelte5)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,7 @@
+import containerQueries from '@tailwindcss/container-queries';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import tailwindcssAnimate from 'tailwindcss-animate';
 import { fontFamily } from "tailwindcss/defaultTheme";
 import type { Config } from "tailwindcss";
 
@@ -59,6 +63,26 @@ const config: Config = {
 			}
 		}
 	},
+  keyframes: {
+    'accordion-down': {
+      from: { height: '0' },
+      to: { height: 'var(--bits-accordion-content-height)' }
+    },
+    'accordion-up': {
+      from: { height: 'var(--bits-accordion-content-height)' },
+      to: { height: '0' }
+    },
+    'caret-blink': {
+      '0%,70%,100%': { opacity: '1' },
+      '20%,50%': { opacity: '0' }
+    }
+  },
+  animation: {
+    'accordion-down': 'accordion-down 0.2s ease-out',
+    'accordion-up': 'accordion-up 0.2s ease-out',
+    'caret-blink': 'caret-blink 1.25s ease-out infinite'
+  },
+  plugins: [typography, forms, containerQueries, tailwindcssAnimate],
 };
 
 export default config;
