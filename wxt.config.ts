e633efc8c22cc4d224ef71d8e6f99407dd96d025
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-04-06 14:22:58 +0700

    refactor: :recycle: configure manifest with wxt

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,5 +8,14 @@ export default defineConfig({
   modules: ['@wxt-dev/module-svelte'],
   alias: {
     $lib: path.resolve('./src/lib')
-  }
+  },
+  runner: {
+    chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
+  },
+  manifestVersion: 3,
+  manifest: {
+    name: 'KMITL DLC',
+    description: "a web extension that add more features to KMITL Registration's website. Forked from KMITL+ by t0ngk.",
+    
+  },
 });
