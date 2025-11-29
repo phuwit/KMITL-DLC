e18f531c5e10360d6adb560fc73ca8affb4b8d59
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-11-29 21:42:47 +0700

    upgrade packages excluding tailwind 4 migration

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,12 +4,11 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  extensionApi: 'chrome',
   modules: ['@wxt-dev/module-svelte'],
   alias: {
     $lib: path.resolve('./src/lib')
   },
-  runner: {
+  webExt: {
     chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
   },
   manifestVersion: 3,
